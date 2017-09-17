48507c466e659509a74e7f74ce44311dddb21757
BrianAmin <tuf49787@temple.edu>
2017-09-17 03:15:37 -0400

    added methods to compute distances from center. added to list, will sort and implemented priority queue system to setup list of targets to hit from closet to farthest. This queue will be passed onto Control.cs

--- FullFrontal_V2/FullFrontal_V2/cv_based.cs
+++ FullFrontal_V2/FullFrontal_V2/cv_based.cs
@@ -13,7 +13,8 @@ using System.Drawing;
 using ArduinoDriver;
 using ArduinoUploader.Hardware;
 using ArduinoUploader;
-
+using Priority_Queue;
+using System.Threading;
 
 namespace FullFrontal_V2
 {
@@ -26,6 +27,17 @@ namespace FullFrontal_V2
         private VideoCapture _capture;
         private CascadeClassifier _cascade;
         DispatcherTimer _timer;
+
+
+
+        //1x2 (x and y coords) ordered based on closet distance to center of barrel
+        public SimplePriorityQueue<Vector> fire_queue;
+
+
+        //frame dimensions for Dell XPS 13 webcam
+        public Vector cam_dim = new Vector(640, 480);
+        //frame dimensions for frank.png test image
+        public Vector image_dim = new Vector(929, 1200);
 
         
 
@@ -48,7 +60,9 @@ namespace FullFrontal_V2
 
             CV_Start.IsEnabled = false;
 
-            _capture = new VideoCapture(cam_number);
+            
+
+            //_capture = new VideoCapture(cam_number);
             _cascade = new CascadeClassifier(@"C:\Users\Merc.MERCURY\Documents\DDNerf\FullFrontal_V2\FullFrontal_V2\haarcascade_frontalface_default.xml");
             _timer = new DispatcherTimer();
 
@@ -69,7 +83,9 @@ namespace FullFrontal_V2
 
         void timer_Tick(object sender, EventArgs e)
         {
-            Mat frame = _capture.QueryFrame();
+            //Mat frame = _capture.QueryFrame();
+
+            Mat frame = CvInvoke.Imread(@"C:\Users\Merc.Mercury\Desktop\franks.png", Emgu.CV.CvEnum.ImreadModes.AnyColor);
 
             if (frame != null)
             {
@@ -77,7 +93,10 @@ namespace FullFrontal_V2
                 CvInvoke.CvtColor(frame, gray, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
                 CvInvoke.EqualizeHist(gray, gray);
                 Rectangle[] faces = _cascade.DetectMultiScale(gray, 1.3, 3, new System.Drawing.Size(gray.Width / 30, gray.Height / 30), new System.Drawing.Size((int)((double)gray.Width / 1.05), (int)((double)gray.Height / 1.05)));
-                
+
+                Thread oThread = new Thread(new ParameterizedThreadStart(priority_sort));
+                oThread.Start(faces);
+
                
                 foreach (Rectangle face in faces)
                 {
@@ -92,6 +111,46 @@ namespace FullFrontal_V2
         }
 
 
+        double distance_formula(double X1, double Y1, double X2, double Y2)
+        {
+            double distance = System.Math.Sqrt(Math.Pow((X2 - X1), 2) + Math.Pow((Y2 - Y1), 2));
+
+
+            return distance;
+        }
+
+
+        //Parameterized ThreadStart Handle
+        private void priority_sort(object faces)
+        {
+            Rectangle[] foobar = (Rectangle[])faces;
+            priority_sort(foobar);
+        }
+
+        //Enqueue Function to sort through faces
+        private void priority_sort(Rectangle[] faces)
+        {
+
+            List<double> raw_list = new List<double>();
+            //image.dim (width, height) or (X,Y)
+            Vector center = new Vector(image_dim.X / 2, image_dim.Y / 2);
+
+           
+            foreach (Rectangle face in faces)
+            {
+
+                double face_distance = distance_formula(image_dim.X, image_dim.Y, face.X, face.Y);
+
+                raw_list.Add(face_distance);
+
+            }
+
+
+            Console.WriteLine(raw_list);
+
+            //A*,enqueue, pass to Control.cs
+            
+        }
         
 
 
